5c9fcb683f4da6c0c60a9d1cdd45307d0217f2e2
maxym Myroshnychenko <mmyros@gmail.com>
2020-12-06 10:40:01 -0500

    seems to work pretty well

--- paperstitch/paperstitch.py
+++ paperstitch/paperstitch.py
@@ -8,6 +8,8 @@ import os
 from tqdm import tqdm
 from warnings import warn
 
+from pathlib import Path
+import shutil
 
 def find_articles(driver, url, pattern):
     print(f'Searching {url} for pattern {pattern}')
@@ -42,11 +44,18 @@ def make_save_folder(url):
         save_folder = f'nature_{url.split("/")[-2]}'
     else:
         save_folder = url[8:25]
-    save_path = f'/home/m/Downloads/{save_folder}/'
-    return save_path, save_folder
+    return save_folder
 
 
-def prep(url=None, use_proxy=False):
+def make_save_path():
+    save_path = Path('/home/m/Downloads/journals_temp/')
+    # Initialize temp directory (make sure it's empty)
+    shutil.rmtree(save_path)
+    Path.mkdir(save_path, exist_ok=True)
+    return str(save_path)
+
+
+def prep(use_proxy=False):
     mime_types = "application/pdf,application/vnd.adobe.xfdf,application/vnd.fdf,application/vnd.adobe.xdp+xml"
     profile = webdriver.FirefoxProfile()
     profile.set_preference('browser.download.folderList', 2)  # custom location
@@ -54,65 +63,29 @@ def prep(url=None, use_proxy=False):
     profile.set_preference('browser.helperApps.neverAsk.saveToDisk', mime_types)
     profile.set_preference("plugin.disable_full_page_plugin_for_types", mime_types)
     profile.set_preference("pdfjs.disabled", True)
-    if url:
-        save_path, save_folder = make_save_folder(url)
-        profile.set_preference('browser.download.dir', save_path)
-    else:
-        save_path, save_folder = None, None
+
+    # Save path (temp folder)
+    save_path = make_save_path()
+    profile.set_preference('browser.download.dir', save_path)
+
     # Make selenium driver
     driver = webdriver.Firefox(profile)
-    driver.get("about:config")
     driver.set_page_load_timeout(8)
     if use_proxy:
         # Log in
         print('Logging you in to ezproxy. I give you 20 seconds')
         driver.get('https://login.ezproxy.nihlibrary.nih.gov/login?url=http://www.nature.com/')
-        time.sleep(20)
-    return driver, save_path, save_folder
+        time.sleep(35)
+    return driver, save_path
 
 
 def save_journal_issue(url="https://browzine.com/libraries/834/journals/13191/issues/369227236?sort=title",
-                       driver=None, save_path=None, save_folder=None):
+                       driver=None, save_path=None):
 
-    def update_save_path(url):
-        def set_string_preference(name, value):
-            driver.execute_script("""
-            var prefs = Components.classes["@mozilla.org/preferences-service;1"]
-                .getService(Components.interfaces.nsIPrefBranch);
-            prefs.setBoolPref(arguments[0], arguments[1]);
-            """, name, value)
-
-        # def set_string_preference(name, value):
-        #     modified = driver.execute_script("""
-        #         document.getElementById("textbox").value = arguments[0];
-        #         FilterPrefs();
-        #         view.selection.currentIndex = 0;
-        #
-        #         if (view.rowCount == 1) {
-        #            current_value = view.getCellText(0, {id:"valeuCol"});
-        #            if (current_value != arguments[1]) {
-        #                ModifySelected();
-        #                return true;
-        #            }
-        #         }
-        #
-        #         return false;
-        #     """, name, value)
-
-            # if modified is None or modified is True:
-            #     alert = driver.switch_to.alert
-            #     alert.send_keys(value)
-            #     alert.accept()
-
-        save_path, save_folder = make_save_folder(url)
-        print(f'Saving to {save_path}')
-        set_string_preference('browser.download.dir', save_path)
-        return save_path, save_folder
     # To prevent download dialog
     if not driver:
-        driver, save_path, save_folder = prep(url)
-    else:
-        save_path, save_folder = update_save_path( url)
+        driver, save_path = prep()
+    save_folder = make_save_folder(url)
     driver.get(url)
     time.sleep(7)
     url = driver.current_url
@@ -155,10 +128,10 @@ def get_fresh_issues():
 
 
 def get_fresh_issues_proxy():
-    driver, save_path, save_folder = prep(use_proxy=False)
+    driver, save_path = prep(use_proxy=True)
     for url in [
-        'https://www.nature.com/neuro/current-issue',
-        'https://www-nature-com/nature/current-issue',
+        # 'https://www.nature.com/neuro/current-issue',
+        # 'https://www.nature.com/nature/current-issue',
         'https://www-nature-com.ezproxy.nihlibrary.nih.gov/neuro/current-issue',
         'https://www-nature-com.ezproxy.nihlibrary.nih.gov/nature/current-issue',
         'https://www-nature-com.ezproxy.nihlibrary.nih.gov/nrn/current-issue',
@@ -166,7 +139,7 @@ def get_fresh_issues_proxy():
         'https://www-nature-com.ezproxy.nihlibrary.nih.gov/npjschz/current-issue',
         'https://www-nature-com.ezproxy.nihlibrary.nih.gov/tp/current-issue',
         'https://science-sciencemag-org.ezproxy.nihlibrary.nih.gov/', ]:
-        save_journal_issue(url, driver, save_path, save_folder)
+        save_journal_issue(url, driver, save_path)
 
     if __name__ == '__main__':
         get_fresh_issues_proxy()
